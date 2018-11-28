5e1e8893225383dd46b98395d3ec19b04459e63a
Nuwan Chamara <nuwanlanka@gmail.com>
2018-11-28 23:15:57 +0530

    Added PageNotFound component

--- jumble/kdnc-react-app/src/components/NavBar.js
+++ jumble/kdnc-react-app/src/components/NavBar.js
@@ -7,6 +7,7 @@ const NavBar = () => (
       <NavLink to="/" className={'navLink'} activeClassName={'activeNavLink'} exact>Home</NavLink>
       <NavLink to="/hello" className={'navLink'} activeClassName={'activeNavLink'}>Hello</NavLink>
       <NavLink to="/prompt" className={'navLink'} activeClassName={'activeNavLink'}>Prompt</NavLink>
+      <NavLink to="/foo" className={'navLink'} activeClassName={'activeNavLink'}>404 Error</NavLink>
     </div>
   </div>
 );
--- jumble/kdnc-react-app/src/routes/index.js
+++ jumble/kdnc-react-app/src/routes/index.js
@@ -4,6 +4,7 @@ import Home from "../components/Home";
 import Hello from "../components/Hello";
 import NavBar from "../components/NavBar";
 import NameForm from "../components/prompt/NameForm";
+import PageNotFound from "../components/PageNotFound";
 
 const routes = (
   <div>
@@ -12,6 +13,7 @@ const routes = (
         <Route exact path="/" component={Home} />
         <Route path="/hello" component={Hello} />
         <Route path="/prompt" component={NameForm} />
+        <Route component={PageNotFound} />
     </Switch>
   </div>
 );
