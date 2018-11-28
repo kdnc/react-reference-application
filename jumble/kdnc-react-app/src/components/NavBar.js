25086f0631c9cbcb7e76bc779722095c42748de0
Nuwan Chamara <nuwanlanka@gmail.com>
2018-11-28 22:12:09 +0530

    Prompt component

--- jumble/kdnc-react-app/src/components/NavBar.js
+++ jumble/kdnc-react-app/src/components/NavBar.js
@@ -6,6 +6,7 @@ const NavBar = () => (
     <div>
       <Link to="/">Home</Link>
       <Link to="/hello">Hello</Link>
+      <Link to="/prompt">Prompt</Link>
     </div>
   </div>
 );
--- jumble/kdnc-react-app/src/routes/index.js
+++ jumble/kdnc-react-app/src/routes/index.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router';
 import Home from "../components/Home";
 import Hello from "../components/Hello";
 import NavBar from "../components/NavBar";
+import NameForm from "../components/prompt/NameForm";
 
 const routes = (
   <div>
@@ -10,6 +11,7 @@ const routes = (
     <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/hello" component={Hello} />
+        <Route path="/prompt" component={NameForm} />
     </Switch>
   </div>
 );
