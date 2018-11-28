a0a71f9c92e9179f989e8fd3afbac2a1f436bb66
Nuwan Chamara <nuwanlanka@gmail.com>
2018-11-28 23:29:11 +0530

    Refactored nav links

--- jumble/kdnc-react-app/src/components/NavBar.js
+++ jumble/kdnc-react-app/src/components/NavBar.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 
-const NavBar = () => (
-  <div>
-    <div>
-      <NavLink to="/" className={'navLink'} activeClassName={'activeNavLink'} exact>Home</NavLink>
-      <NavLink to="/hello" className={'navLink'} activeClassName={'activeNavLink'}>Hello</NavLink>
-      <NavLink to="/prompt" className={'navLink'} activeClassName={'activeNavLink'}>Prompt</NavLink>
-      <NavLink to="/foo" className={'navLink'} activeClassName={'activeNavLink'}>404 Error</NavLink>
+const NavBar = () => {
+
+  const links = [
+    {name: 'Prompt', url: '/prompt'},
+    {name: '404 Error', url: '/404Error'},
+  ];
+
+  let linksComponents = links.map((link, index) => {
+    return (
+      <li key={index} className='nav'>
+        <NavLink className={'navLink'} activeClassName={'activeNavLink'}
+                 to={link.url} exact>{link.name}</NavLink>
+      </li>
+    );
+  });
+
+  return (
+    <div className={'leftNavContainer'}>
+      <ul>
+        <li className='nav'><NavLink className={'navLink'} activeClassName={'activeNavLink'} to='/' exact>Home</NavLink></li>
+        {linksComponents}
+      </ul>
     </div>
-  </div>
-);
+  );
+};
 
 export default NavBar;
--- jumble/kdnc-react-app/src/routes/index.js
+++ jumble/kdnc-react-app/src/routes/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import Home from "../components/Home";
-import Hello from "../components/Hello";
 import NavBar from "../components/NavBar";
 import NameForm from "../components/prompt/NameForm";
 import PageNotFound from "../components/PageNotFound";
@@ -11,7 +10,6 @@ const routes = (
     <NavBar />
     <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/hello" component={Hello} />
         <Route path="/prompt" component={NameForm} />
         <Route component={PageNotFound} />
     </Switch>
